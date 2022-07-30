71899204fb7d18367bad6519193503ef44acc656
JackLin-Yao <jacklinyao0702@163.com>
2022-07-30 19:46:32 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore } from 'vuex'
-
+// 三个模块
+import cart from './modules/cart'
+import user from './modules/user'
+import category from './modules/category'
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    cart,
+    user,
+    category
   }
 })
