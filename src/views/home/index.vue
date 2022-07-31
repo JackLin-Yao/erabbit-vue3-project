441b850f95d5853dd62884de7d40e387188bb6e7
JackLin-Yao <jacklinyao0702@163.com>
2022-08-01 04:17:44 +0800

    左侧分类结构渲染

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="container">
-
+  <!-- 首页入口 -->
+  <div class="home-entry">
+    <div class="container">
+      <!-- 左侧分类 -->
+      <HomeCategory />
+      <!-- 轮播图 -->
+      <!-- <HomeBanner /> -->
+    </div>
   </div>
+  <!-- 新鲜好物 -->
+  <!-- <HomeNew /> -->
+  <!-- 人气推荐 -->
+  <!-- <HomeHot /> -->
+  <!-- 品牌组件 -->
+  <!-- <HomeBrand /> -->
+  <!-- 产品区块 -->
+  <!-- <HomeProduct /> -->
+  <!-- 最新专题 -->
+  <!-- <HomeSpecial /> -->
 </template>
 
 <script>
+import HomeCategory from './components/home-category'
 export default {
   name: 'HomePage',
-  setup () {
-    return {}
-  }
+  components: { HomeCategory }
 }
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
