c6afbd16d974353be619a3626ee719bc1d425bce
JackLin-Yao <jacklinyao0702@163.com>
2022-08-01 05:45:13 +0800

    处理品牌数据渲染

--- src/views/home/components/home-category.vue
+++ src/views/home/components/home-category.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="home-category">
+ <!-- @mouseleave="categoryId=null"把内部数据清空 -->
+  <div class="home-category" @mouseleave="categoryId=null">
     <ul class="menu">
-      <li v-for="item in menuList" :key="item.id">
+      <li
+        :class="{ active: categoryId === item.id }"
+        v-for="item in menuList"
+        :key="item.id"
+        @mouseenter="categoryId = item.id"
+      >
         <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
+        <!-- 初始化时可能没有children的值,用template来进行条件判断 -->
         <template v-if="item.children">
           <RouterLink
             v-for="sub in item.children"
@@ -14,12 +21,46 @@
         </template>
       </li>
     </ul>
+    <!-- 弹出层 -->
+    <div class="layer">
+      <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <ul
+        v-if="currCategory && currCategory.goods && currCategory.goods.length"
+      >
+        <li v-for="item in currCategory.goods" :key="item.id">
+          <RouterLink to="/">
+            <img :src="item.picture" alt="" />
+            <div class="info">
+              <p class="name ellipsis-2">{{ item.name }}</p>
+              <p class="desc ellipsis">{{ item.desc }}</p>
+              <p class="price"><i>¥</i>{{ item.price }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+      <!-- 品牌 -->
+      <ul v-if="currCategory && currCategory.brands">
+        <li class="brand" v-for="brand in currCategory.brands" :key="brand.id">
+          <RouterLink to="/">
+            <img :src="brand.picture" alt="" />
+            <div class="info">
+              <p class="place">
+                <i class="iconfont icon-dingwei"></i>{{ brand.place }}
+              </p>
+              <p class="name ellipsis">{{ brand.name }}</p>
+              <p class="desc ellipsis-2">{{ brand.desc }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import { useStore } from 'vuex'
-import { reactive, computed } from 'vue'
+import { findBrand } from '@/api/home'
+import { reactive, computed, ref } from 'vue'
 export default {
   name: 'HomeCategory',
   // 1. 获取vuex的一级分类，并且只需要两个二级分类
@@ -28,16 +69,16 @@ export default {
   // 4. 进行渲染即可
   setup () {
     const store = useStore()
-    // 最终使用的数据 = 9个分类() + 1个品牌
+    // 最终使用的数据 = 9个分类() + 1个品牌:The final data = 9 categories () + 1 brand
     const brand = reactive({
       id: 'brand',
       name: '品牌',
       children: [{ id: 'brand-children', name: '品牌推荐' }],
-      // 品牌列表
+      // 品牌列表:Brand list
       brands: []
     })
     const menuList = computed(() => {
-      // 得到9个分类切每个一级分类下的子分类只有两个
+      // 得到9个分类切每个一级分类下的子分类只有两个:There are only two sub -categories under each class classification of 9 categories:
       const list = store.state.category.list.map((item) => {
         return {
           id: item.id,
@@ -49,7 +90,17 @@ export default {
       list.push(brand)
       return list
     })
-    return { menuList }
+    // 得到弹出层的推荐商品数据
+    const categoryId = ref(null)
+    const currCategory = computed(() => {
+      return menuList.value.find((item) => item.id === categoryId.value)
+    })
+    // 获取品牌数据，尽量不用使用async再setup上:Get brand data, try not to use async and setup
+    findBrand().then((data) => {
+      brand.brands = data.result
+      console.log(brand.brands)
+    })
+    return { menuList, currCategory, categoryId }
   }
 }
 </script>
@@ -66,7 +117,8 @@ export default {
       padding-left: 40px;
       height: 50px;
       line-height: 50px;
-      &:hover {
+      &:hover,
+      &.active {
         background: @xtxColor;
       }
       a {
@@ -77,6 +129,111 @@ export default {
         }
       }
     }
+  }
+  // 弹出层样式
+  .layer {
+    width: 990px;
+    height: 500px;
+    background: rgba(255, 255, 255, 0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+        a {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+          &:hover {
+            background: #e3f9f4;
+          }
+          img {
+            width: 95px;
+            height: 95px;
+          }
+          .info {
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+            .desc {
+              color: #999;
+            }
+            .price {
+              font-size: 22px;
+              color: @priceColor;
+              i {
+                font-size: 16px;
+              }
+            }
+          }
+        }
+      }
+      // 品牌的样式
+      li.brand {
+        height: 180px;
+        a {
+          align-items: flex-start;
+          img {
+            width: 120px;
+            height: 160px;
+          }
+          .info {
+            p {
+              margin-top: 8px;
+            }
+            .place {
+              color: #999;
+            }
+          }
+        }
+      }
+    }
+  }
+  &:hover {
+    .layer {
+      display: block;
+    }
+  }
+}
+// 骨架动画
+.xtx-skeleton {
+  animation: fade 1s linear infinite alternate;
+}
+@keyframes fade {
+  from {
+    opacity: 0.2;
+  }
+  to {
+    opacity: 1;
   }
 }
 </style>
